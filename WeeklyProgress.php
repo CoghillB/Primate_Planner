9c03cd4bd657c9c68c9c5b36ef840740d1aeac64
braydenc <coghill30@gmail.com>
2024-12-04 14:24:48 -0800

    Update database name for mysqli connections.
    Changed the database name from 'fitnesstracker' to 'Primate_Planner' in both WeeklyProgress.php and FitnessTracker.php files. This modification ensures that connections are made to the correct database, which is crucial for retrieving and storing data accurately.

--- WeeklyProgress.php
+++ WeeklyProgress.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 //  connection
-$conn = new mysqli('localhost', 'cs213user', 'letmein', 'fitnesstracker');
+$conn = new mysqli('localhost', 'cs213user', 'letmein', 'Primate_Planner');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
